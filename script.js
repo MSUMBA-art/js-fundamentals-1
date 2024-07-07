e14f204895acdcf509623c915d137b36784fae61
ALEXANDER <msumba83@gmail.com>
2024-07-07 14:43:14 +0200

    values and valuables

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let js = 'amazing';
 
-console.log(40 + 4 - 16);
+console.log(40 + 48 + 23 - 16);
+
+console.log('Alexander');
+
+console.log(23);
+
+let firstName = 'Alexander';
+
+console.log(firstName.length);
+console.log(firstName);
+console.log(firstName);

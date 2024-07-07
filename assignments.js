6406faf8b37c87bacaca7c94d44f310a5d22a9d1
ALEXANDER <msumba83@gmail.com>
2024-07-07 15:13:29 +0200

    initial commit

---
+++ assignments.js
@@ -0,0 +1,14 @@
+// https://codingheroes.io/assignments/values-and-variables
+
+// Values and Variables
+
+// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// 2. Log their values to the console.
+let country = 'South Africa';
+let continent = 'Africa';
+let population = 64000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);

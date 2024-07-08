52397eb08458846c56affe986cc5cbb49461d3b9
ALEXANDER <msumba83@gmail.com>
2024-07-08 06:10:49 +0200

    initial commit

--- assignments.js
+++ assignments.js
@@ -5,10 +5,10 @@
 // 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 
 // 2. Log their values to the console.
-let country = 'South Africa';
-let continent = 'Africa';
-let population = 64000000;
+// let country = 'South Africa';
+// let continent = 'Africa';
+// let population = 64000000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let js = 'amazing';
+// let js = 'amazing';
 
-console.log(40 + 48 + 23 - 16);
+// console.log(40 + 48 + 23 - 16);
 
-console.log('Alexander');
+// console.log('Alexander');
 
-console.log(23);
+// console.log(23);
 
-let firstName = 'Alexander';
+// let firstName = 'Alexander';
 
-console.log(firstName.length);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName.length);
+// console.log(firstName);
+// console.log(firstName);
+
+// DATA TYPES
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 33);
+console.log(typeof 'Alex');
+console.log(typeof true);
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);

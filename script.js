48947c5ca4dab56f244f52d53c09be032da12816
ALEXANDER <msumba83@gmail.com>
2024-07-14 19:59:30 +0200

    done if/else statement

--- script.js
+++ script.js
@@ -104,3 +104,14 @@ if (age >= 18) {
     const yearsLeft = 18 - age;
     console.log(`Alexander is too young, wait another ${yearsLeft} years`);
  }
+
+const birthYear = 2012;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21
+}
+
+console.log(century);
